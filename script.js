7336a05fdb49b3dfe62449645e00e6340cf71b20
tyisyustat24 <tyis.yustat24@icloud.com>
2021-10-29 09:10:09 -0700

    did all of my js and connected it to html

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('hello I am Alive');
+
+document.write("<h1>Conditionals-and-Operators</h1>");
+
+var numb = prompt('choose a random number 1-100');
+
+if( numb = 51){
+    alert('')
+}
+
+else if(numb < 75 && numb > 25) {
+    alert('your number is between 25 and 75 isnt it');
+}
+else if(numb > 75){
+    alert('Your number is greater than 75 isnt it');
+}
+else if(numb <  25){
+    alert('your number is less than 25 isnt it');
+}
+
+document.write("<h2>your number is<h2>");
+document.write(numb); 
